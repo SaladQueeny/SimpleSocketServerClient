53d97a5cf7ddc2e09e0720a29bb5de9aa72407ee
SaladQueeny <kolpakov.egor@icloud.com>
2021-10-06 23:26:45 +0300

    Trying to install javafx in Client

--- Client/src/main/java/Client.java
+++ Client/src/main/java/Client.java
@@ -1,19 +1,27 @@
-import general.Generalities;
+
 import java.io.*;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Client {
-    public static void main(String[] args) {
-        try(Generalities generalities = new Generalities("127.0.0.1", 8080)){
-            System.out.println("Connected to server");
-            generalities.writeLine(ProcessData.createRequest());
-            String response = generalities.readLine();
-            System.out.println("Response: "+response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void goTo(){
+//        public static void main(String[] args) {
+            try(GeneralThings generalThings = new GeneralThings("127.0.0.1", 8080)){
+                System.out.println("Connected to server");
+                generalThings.writeLine(ProcessData.createRequest());
+                String response = generalThings.readLine();
+                System.out.println("Response: "+response);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            try(Generalities generalities = new Generalities("127.0.0.1", 8080)){
+//                System.out.println("Connected to server");
+//                generalities.writeLine(ProcessData.createRequest());
+//                String response = generalities.readLine();
+//                System.out.println("Response: "+response);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
 
+//        }
     }
+
 }
