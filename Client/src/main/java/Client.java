d503d5314ed2353ac66c2e7062102822904cf78c
SaladQueeny <kolpakov.egor@icloud.com>
2021-10-07 19:01:52 +0300

    Good version

--- Client/src/main/java/Client.java
+++ Client/src/main/java/Client.java
@@ -1,25 +1,27 @@
 
+import general.Generalities;
+
 import java.io.*;
 
 public class Client {
-    public void goTo(){
-//        public static void main(String[] args) {
-            try(GeneralThings generalThings = new GeneralThings("127.0.0.1", 8080)){
-                System.out.println("Connected to server");
-                generalThings.writeLine(ProcessData.createRequest());
-                String response = generalThings.readLine();
-                System.out.println("Response: "+response);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-//            try(Generalities generalities = new Generalities("127.0.0.1", 8080)){
+//    public void goTo(){
+        public static void main(String[] args) {
+//            try(GeneralThings generalThings = new GeneralThings("127.0.0.1", 8080)){
 //                System.out.println("Connected to server");
-//                generalities.writeLine(ProcessData.createRequest());
-//                String response = generalities.readLine();
+//                generalThings.writeLine(ProcessData.createRequest());
+//                String response = generalThings.readLine();
 //                System.out.println("Response: "+response);
 //            } catch (IOException e) {
 //                e.printStackTrace();
 //            }
+            try(Generalities generalities = new Generalities("127.0.0.1", 8080)){
+                System.out.println("Connected to server");
+                generalities.writeLine(ProcessData.createRequest());
+                String response = generalities.readLine();
+                System.out.println("Response: "+response);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
 //        }
     }
