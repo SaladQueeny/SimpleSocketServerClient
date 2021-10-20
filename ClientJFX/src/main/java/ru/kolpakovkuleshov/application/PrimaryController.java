9d43b61a631444e61966d8bef6e78715fdfb2a18
piNine <vanyakuleshov@gmail.com>
2021-10-20 19:10:31 +0300

    Client already done! All alerts ready.

--- ClientJFX/src/main/java/ru/kolpakovkuleshov/application/PrimaryController.java
+++ ClientJFX/src/main/java/ru/kolpakovkuleshov/application/PrimaryController.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
@@ -59,62 +62,65 @@ public class PrimaryController {
     @FXML
     private TextField ystart;
 
-
     private void sendRequestToServer() throws IOException {
         try (Generalities generalities = new Generalities("127.0.0.1", 8080)) {
             generalities.writeLine(ProcessData.createRequest());
             String response = generalities.readLine();
             ProcessData.getDataFromJson(response);
         } catch (IOException e) {
+            Alert a1 = new Alert(Alert.AlertType.ERROR);
+            a1.setTitle("ERROR");
+            a1.setContentText("Can't join to server!");
+            a1.setHeaderText("Server error!");
+            a1.show();
             e.printStackTrace();
         }
     }
 
     private boolean isGoodData() {
-//        Pattern p = Pattern.compile("(\\d+)([.]*)(\\d*)");
-//        Matcher match = p.matcher(xstart.getText().trim());
-//        int check =0;
-//        if(match.matches()){
-//            check++;
-//            match = p.matcher(xend.getText().trim());
-//            if(match.matches()){
-//                check++;
-//                match = p.matcher(xchange.getText().trim());
-//                if(match.matches()){
-//                    check++;
-//                    match = p.matcher(ystart.getText().trim());
-//                    if(match.matches()){
-//                        check++;
-//                        match = p.matcher(yend.getText().trim());
-//                        if(match.matches()){
-//                            check++;
-//                            match = p.matcher(ychange.getText().trim());
-//                            if(match.matches()){
-//                                check++;
-//                                match = p.matcher(tstart.getText().trim());
-//                                if(match.matches()){
-//                                    check++;
-//                                    match = p.matcher(tend.getText().trim());
-//                                    if(match.matches()){
-//                                        check++;
-//                                        match = p.matcher(ychange.getText().trim());
-//                                        if(match.matches()){
-//                                            check++;
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        if(check==9){
-//            return true;
-//        }else{
-//            return false;
-//        }
-        return true;
+        Pattern p = Pattern.compile("[-]?[0-9]+(.[0-9]+)?");
+        Matcher match = p.matcher(xstart.getText().trim());
+        int check =0;
+        if(match.matches()){
+            check++;
+            match = p.matcher(xend.getText().trim());
+            if(match.matches()){
+                check++;
+                match = p.matcher(xchange.getText().trim());
+                if(match.matches()){
+                    check++;
+                    match = p.matcher(ystart.getText().trim());
+                    if(match.matches()){
+                        check++;
+                        match = p.matcher(yend.getText().trim());
+                        if(match.matches()){
+                            check++;
+                            match = p.matcher(ychange.getText().trim());
+                            if(match.matches()){
+                                check++;
+                                match = p.matcher(tstart.getText().trim());
+                                if(match.matches()){
+                                    check++;
+                                    match = p.matcher(tend.getText().trim());
+                                    if(match.matches()){
+                                        check++;
+                                        match = p.matcher(tchange.getText().trim());
+                                        if(match.matches()){
+                                            check++;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(check==9){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     private void getStartData() {
@@ -135,7 +141,7 @@ public class PrimaryController {
 
     public boolean check = false;
     List<ImageView> imageViewList = new ArrayList<>();
-
+    public double previous_T;
     @FXML
     void initialize() {
         Logs.writeLog(this.getClass(), "initialize", Level.INFO);
@@ -155,13 +161,28 @@ public class PrimaryController {
                     chart_pain.getChildren().add(imageViewList.get(ProcessData.t.indexOf(currentT)));
                 } else {
                     //notification
-                    System.out.println("We don't have this T");
+                    Alert a1 = new Alert(Alert.AlertType.ERROR);
+                    a1.setTitle("ERROR");
+                    a1.setContentText("We don't have this T!");
+                    a1.setHeaderText("Incorrect data!");
+                    a1.show();
+                }
+                if (previous_T != currentT){
+
+                }else{
+                    Alert a1 = new Alert(Alert.AlertType.WARNING);
+                    a1.setTitle("WARNING");
+                    a1.setContentText("We have this T!");
+                    a1.setHeaderText("Duplicate T!");
+                    a1.show();
                 }
                 check = false;
+                previous_T = currentT;
             }
 
         });
         start_button.setOnAction(event -> {
+
             if (isGoodData()) {
                 getStartData();
                 try {
@@ -179,13 +200,22 @@ public class PrimaryController {
                     chart_pain.getChildren().add(imageViewList.get(0));
                 } catch (IOException e) {
                     //notification
+                    Alert a1 = new Alert(Alert.AlertType.ERROR);
+                    a1.setTitle("ERROR");
+                    a1.setContentText("Can't join to server!");
+                    a1.setHeaderText("Server error!");
+                    a1.show();
                     System.out.println("Something wrong with server");
                     e.printStackTrace();
                 }
                 System.out.println("We got your data");
             } else {
                 //notification
-                System.out.println("Incorrect data");
+                Alert a1 = new Alert(Alert.AlertType.ERROR);
+                a1.setTitle("ERROR");
+                a1.setContentText("The data entered is incorrect or there is not enough data!");
+                a1.setHeaderText("Incorrect data!");
+                a1.show();
             }
 
         });
