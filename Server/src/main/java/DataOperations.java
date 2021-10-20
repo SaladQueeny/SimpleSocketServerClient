9c9bea41987e316f4cff397fc15202c85b37a86f
SaladQueeny <kolpakov.egor@icloud.com>
2021-10-20 18:22:17 +0300

    All done except logger and pattern

--- Server/src/main/java/DataOperations.java
+++ Server/src/main/java/DataOperations.java
@@ -10,27 +10,28 @@ public class DataOperations {
     public List<Double> y = new ArrayList<>();
     public List<Double> x = new ArrayList<>();
     public List<Double> t = new ArrayList<>();
-    public void getAndSetData(String JSONdata)  {
+
+    public void getAndSetData(String JSONdata) {
         try {
             JSONParser parser = new JSONParser();
             JSONObject obj = (JSONObject) parser.parse(JSONdata);
-            double x_start= (double) obj.get("x_start"),
-                    x_end=(double) obj.get("x_end"),
-                    x_change=(double) obj.get("x_change"),
-                    y_start=(double) obj.get("y_start"),
-                    y_end=(double) obj.get("y_end"),
-                    y_change=(double) obj.get("y_change"),
-                    t_start=(double) obj.get("t_start"),
-                    t_end=(double) obj.get("t_end"),
-                    t_change=(double) obj.get("t_change");
-            for(double xyt=x_start;xyt<=x_end; xyt+=x_change){
-                x.add(Math.round(xyt*100000000)/100000000.0);
+            double x_start = (double) obj.get("x_start"),
+                    x_end = (double) obj.get("x_end"),
+                    x_change = (double) obj.get("x_change"),
+                    y_start = (double) obj.get("y_start"),
+                    y_end = (double) obj.get("y_end"),
+                    y_change = (double) obj.get("y_change"),
+                    t_start = (double) obj.get("t_start"),
+                    t_end = (double) obj.get("t_end"),
+                    t_change = (double) obj.get("t_change");
+            for (double xyt = x_start; xyt <= x_end; xyt += x_change) {
+                x.add(Math.round(xyt * 10000) / 10000.0);
             }
-            for(double xyt=y_start;xyt<=y_end; xyt+=y_change){
-                y.add(Math.round(xyt*100000000)/100000000.0);
+            for (double xyt = y_start; xyt <= y_end; xyt += y_change) {
+                y.add(Math.round(xyt * 10000) / 10000.0);
             }
-            for(double xyt=t_start;xyt<=t_end; xyt+=t_change){
-                t.add(Math.round(xyt*100000000)/100000000.0);
+            for (double xyt = t_start; xyt <= t_end; xyt += t_change) {
+                t.add(Math.round(xyt * 10000) / 10000.0);
             }
         } catch (ParseException e) {
             e.printStackTrace();
@@ -42,9 +43,9 @@ public class DataOperations {
         getAndSetData(data);
         List<List<List<Double>>> z = ComputeFunction.calculate(x, y, t);
         JSONArray resultMain = new JSONArray();
-        for(int i=0; i< z.size();i++){
+        for (int i = 0; i < z.size(); i++) {
             JSONArray result = new JSONArray();
-            for (int j=0; j< z.get(i).size();j++){
+            for (int j = 0; j < z.get(i).size(); j++) {
                 result.add(z.get(i).get(j));
             }
             resultMain.add(result);
@@ -53,19 +54,19 @@ public class DataOperations {
         object.put("z", resultMain);
 
         resultMain = new JSONArray();
-        for(int i=0; i< t.size();i++){
+        for (int i = 0; i < t.size(); i++) {
             resultMain.add(t.get(i));
         }
         object.put("t", resultMain);
 
         resultMain = new JSONArray();
-        for(int i=0; i< x.size();i++){
+        for (int i = 0; i < x.size(); i++) {
             resultMain.add(x.get(i));
         }
         object.put("x", resultMain);
 
         resultMain = new JSONArray();
-        for(int i=0; i< y.size();i++){
+        for (int i = 0; i < y.size(); i++) {
             resultMain.add(y.get(i));
         }
         object.put("y", resultMain);
