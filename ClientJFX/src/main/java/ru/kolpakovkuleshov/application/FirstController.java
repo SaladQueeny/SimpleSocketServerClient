754cc427e8a554e0bec2085209b043ae2d61b602
piNine <vanyakuleshov@gmail.com>
2021-10-25 21:02:27 +0300

    First scene added

---
+++ ClientJFX/src/main/java/ru/kolpakovkuleshov/application/FirstController.java
@@ -0,0 +1,15 @@
+package ru.kolpakovkuleshov.application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class FirstController {
+
+    @FXML
+    private Button start_path;
+
+    @FXML
+    private TextField textFieldPath;
+
+}
