15ee4205719b8a5f51235cfd19e5d14a69180019
SaladQueeny <kolpakov.egor@icloud.com>
2021-11-07 22:01:13 +0300

    add multithreading in primary controller

--- ClientJFX/src/main/java/ru/kolpakovkuleshov/application/SecondaryController.java
+++ ClientJFX/src/main/java/ru/kolpakovkuleshov/application/SecondaryController.java
@@ -1,13 +1,70 @@
 package ru.kolpakovkuleshov.application;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
-import ru.kolpakovkuleshov.App;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 public class SecondaryController {
+    @FXML
+    private ResourceBundle resources;
 
     @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+    private URL location;
+
+    @FXML
+    private Button change_path_button;
+
+    @FXML
+    private Pane chart_pain;
+
+    @FXML
+    private Button exit_button;
+
+    @FXML
+    private Slider slider_chart;
+
+    @FXML
+    private Button start_button;
+
+    @FXML
+    private TextField tchange;
+
+    @FXML
+    private TextField tend;
+
+    @FXML
+    private TextField tstart;
+
+    @FXML
+    private Button update_button;
+
+    @FXML
+    private TextField xchange;
+
+    @FXML
+    private TextField xend;
+
+    @FXML
+    private TextField xstart;
+
+    @FXML
+    private TextField ychange;
+
+    @FXML
+    private TextField yend;
+
+    @FXML
+    private TextField ystart;
+
+    @FXML
+    void initialize() {
+        start_button.setDisable(false);
+
     }
+
 }
